2f82356d8f2d9f8ef8584b8b136c9a44c25d9d1e
Nicolas Alvaredo <nicoalvaredo_utn@hotmail.com>
2024-10-14 14:47:27 -0300

    Actualizo urls

--- mi_app/urls.py
+++ mi_app/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.inicio, name='inicio'),
-    path('crear_pelicula/', views.crear_pelicula, name='crear_pelicula'),
-    path('buscar_peliculas/', views.buscar_peliculas, name='buscar_peliculas'),
-    path('acerca_de_mi/', views.acerca_de_mi, name='acerca_de_mi'),
+    path('crear-pelicula/', views.crear_pelicula, name='crear_pelicula'),
+    path('buscar-peliculas/', views.buscar_peliculas, name='buscar_peliculas'),
+    path('acerca-de-mi/', views.acerca_de_mi, name='acerca_de_mi'),
 ]
