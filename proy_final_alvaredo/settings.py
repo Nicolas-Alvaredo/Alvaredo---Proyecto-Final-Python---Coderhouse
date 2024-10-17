dfc6566d9c29faf771ec381592a6db82d8ad2f46
Nicolas Alvaredo <nicoalvaredo_utn@hotmail.com>
2024-10-17 19:41:52 -0300

    Templates base update

--- proy_final_alvaredo/settings.py
+++ proy_final_alvaredo/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'proy_final_alvaredo.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
