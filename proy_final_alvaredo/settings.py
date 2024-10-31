892687d7b56fe3b34ca7b8abd7ca7584e20a4c12
Nicolas Alvaredo <nicoalvaredo_utn@hotmail.com>
2024-10-31 15:54:33 -0300

    Agregados settings

--- proy_final_alvaredo/settings.py
+++ proy_final_alvaredo/settings.py
@@ -123,9 +123,11 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'mi_app/static'),
 ]
+
 
 
 # Default primary key field type
