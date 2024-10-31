0d3c25e77014dc8ec8bdd1b271332ff404ae799a
Nicolas Alvaredo <nicoalvaredo_utn@hotmail.com>
2024-10-31 15:40:49 -0300

    Actualizacion settings

--- proy_final_alvaredo/settings.py
+++ proy_final_alvaredo/settings.py
@@ -123,6 +123,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'mi_app/static'),
+]
 
 
 # Default primary key field type
